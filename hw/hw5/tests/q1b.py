c8c3c2ab98dd3103041887db630e70b8378a26b6
Rohil Kanwar <rohilkanwar@berkeley.edu>
2021-07-08 09:21:54 -0700

    hw5 assignment released

---
+++ hw/hw5/tests/q1b.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q1b',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> 1 <= num_holidays <= 10000\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
