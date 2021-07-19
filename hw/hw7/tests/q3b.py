0a15b7a097ef5abec0a3d6827f37ec407469a8ae
Rohil Kanwar <rohilkanwar@berkeley.edu>
2021-07-19 03:16:23 -0700

    adding hw7 materials

---
+++ hw/hw7/tests/q3b.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3b',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> ('Log Building Square Feet' in training_data.columns) == True\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': ">>> np.isclose(training_data['Log Building Square Feet'].mean(), 7.2914293261161)\nTrue", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
