1a689894df9e73c65d0d4fd5c20375b7bf354825
Michelle Li <michelleli@michelles-air.myfiosgateway.com>
2021-07-25 13:15:47 -0400

    adding lab11

---
+++ lab/lab11/tests/q2b.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q2b',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': ">>> decision_tree_model.criterion == 'gini'\nTrue", 'hidden': False, 'locked': False},
+                                   {'code': '>>> decision_tree_model.random_state == 42\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
