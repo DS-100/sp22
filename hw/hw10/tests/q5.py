d2e46dc0e6afa0bb33a3949948e8190830da05ec
Default Jupyter user <ischmidt20@berkeley.edu>
2021-07-29 13:38:18 -0700

    actual hw10 release

---
+++ hw/hw10/tests/q5.py
@@ -0,0 +1,1 @@
+test = {'name': 'q5', 'points': 2, 'suites': [{'cases': [{'code': '>>> training_accuracy > 0.72\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ hw/hw10/tests/q6d.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q6d',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> logistic_predictor_precision >= 0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> logistic_predictor_recall >= 0\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> logistic_predictor_far >= 0\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
