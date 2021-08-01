02c37b32226d399303883dfad522f376d8bb5a11
Michelle Li <michelleli@michelles-air.myfiosgateway.com>
2021-08-01 15:46:27 -0400

    adding labs12 and 13

---
+++ lab/lab13/tests/q2b.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2b',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> sorted(np.unique(agg_single.labels_, return_counts = True)[1]) == [25, 475]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab13/tests/q3.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q3',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> sorted(np.unique(spectral.labels_, return_counts = True)[1]) == [42, 170, 174]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
