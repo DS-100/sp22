3804d417b493b61c0deef90076df99a66a327b5d
andrewlenz <andrew.lenz@berkeley.edu>
2021-09-02 17:23:15 -0700

    added lab02

---
+++ lab/lab02/tests/q3.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': ">>> fruit_info_caps.columns\nIndex(['Fruit', 'Color'], dtype='object')", 'hidden': False, 'locked': False},
+                                   {'code': ">>> fruit_info.columns\nIndex(['fruit', 'color', 'rank1', 'rank2'], dtype='object')", 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab02/tests/q4.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q4',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> name_and_year.shape\n(6122890, 2)', 'hidden': False, 'locked': False},
+                                   {'code': '>>> name_and_year.loc[0, "Name"]\n\'Mary\'', 'hidden': False, 'locked': False},
+                                   {'code': '>>> name_and_year.loc[0, "Year"]\n1910', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
