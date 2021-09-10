1e163f5451ebb968b1e6de0c71c67316a3946827
andrewlenz <andrew.lenz@berkeley.edu>
2021-09-09 21:13:30 -0700

    hw3 added to public

---
+++ hw/hw3/tests/q2ci.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2ci',
+    'points': 1,
+    'suites': [   {   'cases': [{'code': '>>> assert valid_zips.dtype == object \n>>> type(valid_zips) == pd.Series\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
