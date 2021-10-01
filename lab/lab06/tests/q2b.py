b31146db48a83ee786d546eec5845893e6725879
andrewlenz <andrew.lenz@berkeley.edu>
2021-09-30 23:57:30 -0700

    lab06 added

---
+++ lab/lab06/tests/q2b.py
@@ -0,0 +1,1 @@
+test = {'name': 'q2b', 'points': None, 'suites': [{'cases': [{'code': '>>> min_observed_mse\n3', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
