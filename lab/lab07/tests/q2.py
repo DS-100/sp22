fc40e873e2191f8574bbd885991ce8bbb2f0d5e8
andrewlenz <andrew.lenz@berkeley.edu>
2021-10-07 18:11:16 -0700

    lab07 added

---
+++ lab/lab07/tests/q2.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> np.isclose(l2_tip_risk_list([0, 0]), 10.896283606557375)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab07/tests/q4a.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q4a',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> np.isclose(model_multiple.intercept_, 56.900099702112954)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(model_multiple.coef_[0], -0.46618963)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
