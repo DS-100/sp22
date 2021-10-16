cec138da0078863e0e72cc8643fa06f2c7ce1745
andrewlenz <andrew.lenz@berkeley.edu>
2021-10-15 17:54:10 -0700

    hw8 added

---
+++ hw/hw8/tests/q5a.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q5a',
+    'points': [0],
+    'suites': [{'cases': [{'code': '>>> isinstance(q5a, int) == True\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
