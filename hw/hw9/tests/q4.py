92c9ff017f175311e5a580368c9c8f763fb2addf
andrewlenz <andrew.lenz@berkeley.edu>
2021-10-22 17:54:30 -0700

    hw9 posted

---
+++ hw/hw9/tests/q4.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q4',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> np.isclose(y_fitted_m1.max(), 17.528601849438104, atol=0.0001) == True\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(y_fitted_m2.max(), 15.614096224439168, atol=0.0001) == True\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(y_predicted_m1.max(), 15.540922864181525, atol=0.0001) == True\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(y_predicted_m2.max(), 15.02563963305767, atol=0.0001) == True\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw9/tests/q8.py
@@ -0,0 +1,30 @@
+test = {   'name': 'q8',
+    'points': 6,
+    'suites': [   {   'cases': [   {   'code': ">>> run_linear_regression_test(lm.LinearRegression(fit_intercept=True), process_data_fm, 80000, 'cook_county_train.csv', None, False)\nTrue",
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> run_linear_regression_test(lm.LinearRegression(fit_intercept=True), process_data_fm, 160000, 'cook_county_train.csv', None, False)\nTrue",
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> run_linear_regression_test(lm.LinearRegression(fit_intercept=True), process_data_fm, 260000, 'cook_county_train.csv', None, False)\nTrue",
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> run_linear_regression_test(lm.LinearRegression(fit_intercept=True), process_data_fm, 85000, 'cook_county_train.csv', "
+                                               "'cook_county_test_kaggle.csv', False)\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> run_linear_regression_test(lm.LinearRegression(fit_intercept=True), process_data_fm, 165000, 'cook_county_train.csv', "
+                                               "'cook_county_test_kaggle.csv', False)\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> run_linear_regression_test(lm.LinearRegression(fit_intercept=True), process_data_fm, 265000, 'cook_county_train.csv', "
+                                               "'cook_county_test_kaggle.csv', False)\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
