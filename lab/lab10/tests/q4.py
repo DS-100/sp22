e3a59cb480117f420d1f0920e746bbd6f47b6366
andrewlenz <andrew.lenz@berkeley.edu>
2021-10-22 17:44:46 -0700

    lab10 posted

---
+++ lab/lab10/tests/q4.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q4',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': ">>> np.isclose(compute_CV_error(lm.LinearRegression(), X_train2[['TAX', 'INDUS', 'CRIM']],Y_train2), 7.761704927406191)\nTrue",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab10/tests/q6.py
@@ -0,0 +1,1 @@
+test = {'name': 'q6', 'points': None, 'suites': [{'cases': [{'code': '>>> test_rmse <= 5.2\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
