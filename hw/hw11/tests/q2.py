fbadb284814ba1d19a3256c70697c5fa15844ea6
andrewlenz <andrew.lenz@berkeley.edu>
2021-11-12 21:07:24 -0800

    hw 11 materials added

---
+++ hw/hw11/tests/q2.py
@@ -0,0 +1,13 @@
+test = {   'name': 'q2',
+    'points': 4,
+    'suites': [   {   'cases': [   {'code': ">>> x, y = part_1_data['x'], part_1_data['y']\n>>> abs(sin_MSE([0, np.pi], x, y) - 19.49000412080223) <= 1e-5\nTrue", 'hidden': False, 'locked': False},
+                                   {   'code': ">>> x, y = part_1_data['x'], part_1_data['y']\n>>> abs(sin_MSE_dt1([0, np.pi], x, y) - -25.376660670924529) <= 1e-5\nTrue",
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': ">>> x, y = part_1_data['x'], part_1_data['y']\n>>> abs(sin_MSE_dt2([0, np.pi], x, y) - 1.9427210155296564) <= 1e-5\nTrue",
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw11/tests/q3b.py
@@ -0,0 +1,12 @@
+test = {   'name': 'q3b',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': '>>> len(thetas_used_decay) == len(losses_calculated_decay) == 21 # theta history and loss history are 20 items in them\nTrue',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> thetas_used_decay[0].shape == (2,) # theta history contains theta values\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isscalar(losses_calculated_decay[0]) # loss history should be a list of values, not vector\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> losses_calculated_decay[1] - losses_calculated_decay[-1] > 0 # loss is decreasing\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
