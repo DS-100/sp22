4fc8a6ae4fa1b37042b4d69c240a47e8a74bd669
andrewlenz <andrew.lenz@berkeley.edu>
2021-11-27 17:08:21 -0800

    lab 14 added

---
+++ lab/lab14/tests/q1.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q1',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> sorted(np.unique(kmeans_q1.labels_, return_counts = True)[1]) == [12, 13, 50]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab14/tests/q2a.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q2a',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> sorted(np.unique(kmeans_q2a.labels_, return_counts = True)[1]) == [45, 455]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
