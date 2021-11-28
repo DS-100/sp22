7ac401b47ab7457f5d13408499a2b7b6e38d8ddb
andrewlenz <andrew.lenz@berkeley.edu>
2021-11-28 14:50:07 -0800

    lab 15 added

---
+++ lab/lab15/tests/q3.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q3',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> sorted(np.unique(spectral.labels_, return_counts = True)[1]) == [42, 170, 174]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab15/tests/q4.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q4',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> sorted(np.unique(kmeans_q4.labels_, return_counts = True)[1]) == [3, 25, 90, 99]\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
