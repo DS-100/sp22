c71ef39fff30ecc50e55883ff873ceff00e21a74
andrewlenz <andrew.lenz@berkeley.edu>
2021-11-30 07:45:08 -0800

    hw13 materials added

---
+++ hw/hw13/tests/q4.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q4',
+    'points': 3,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(test_predictions, np.ndarray) # must be ndarray of predictions\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> np.array_equal(np.unique(test_predictions), np.array([0, 1])) # must be binary labels (0 or 1) and not probabilities\nTrue',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> len(test_predictions) == 1000 # must be the right number of predictions\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
