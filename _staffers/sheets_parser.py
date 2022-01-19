aed3049a1feeaddcc392cab5a33767e1afc28da6
Dominic Liu <dominiclhx129@gmail.com>
2022-01-18 21:11:26 -0800

    fix file name

--- _staffers/sheets_parser.py
+++ _staffers/sheets_parser.py
@@ -78,7 +78,7 @@ def main():
                 + 'role: ' + attributes['role'] + '\n'
                 + 'email: ' + attributes['email'] + '\n'
                 + 'website: ' + attributes['website'] + '\n'
-                + 'photo: http://ds100.org/fa21/resources/assets/staff_pics/' + get_photo_location(photos, attributes) + '\n'
+                + 'photo: http://ds100.org/sp22/resources/assets/staff_pics/' + get_photo_location(photos, attributes) + '\n'
                 + 'pronouns: ' + attributes['pronouns'] + '\n'
                 + '---\n'
                 + attributes['bio'] + '\n')
