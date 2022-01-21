434f2b26b3e4d5c0adede95e96fa3bb89c5517a4
Dominic Liu <dominiclhx129@gmail.com>
2022-01-20 21:06:51 -0800

    lab01

---
+++ lab/lab01/tests/q2b.py
@@ -0,0 +1,13 @@
+test = {   'name': 'q2b',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> elementwise_array_sum([], [])\narray([], dtype=float64)', 'hidden': False, 'locked': False},
+                                   {'code': '>>> elementwise_array_sum([1], [1])\narray([2])', 'hidden': False, 'locked': False},
+                                   {'code': '>>> elementwise_array_sum([-1], [1])\narray([2])', 'hidden': False, 'locked': False},
+                                   {'code': '>>> elementwise_array_sum([1], [-1])\narray([0])', 'hidden': False, 'locked': False},
+                                   {'code': '>>> elementwise_array_sum([1, 2, 3], [1, 2, 3])\narray([ 2, 12, 36])', 'hidden': False, 'locked': False},
+                                   {'code': '>>> elementwise_array_sum([1, 5, 2], [3, 6, 6])\narray([ 28, 241, 220])', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(elementwise_array_sum([], [])) is np.ndarray\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab01/tests/q2d.py
@@ -0,0 +1,17 @@
+test = {   'name': 'q2d',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> valid_values\n'
+                                               'array([0.95071431, 0.86617615, 0.96990985, 0.94888554, 0.96563203,\n'
+                                               '       0.9093204 , 0.96958463, 0.93949894, 0.89482735, 0.92187424])',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {'code': '>>> len(valid_values) == 10\nTrue', 'hidden': False, 'locked': False},
+                                   {   'code': '>>> np.allclose(valid_values, [0.95071431, 0.86617615, 0.96990985, 0.94888554, 0.96563203,\n'
+                                               '...        0.9093204 , 0.96958463, 0.93949894, 0.89482735, 0.92187424])\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
