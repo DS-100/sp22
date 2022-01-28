b0ec8beb215f127afb8b355805200ca35297aa54
Dominic Liu <dominiclhx129@gmail.com>
2022-01-28 12:25:05 -0800

    hw02

---
+++ hw/hw02/tests/q2a.py
@@ -0,0 +1,9 @@
+test = {   'name': 'q2a',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> type(zip_counts) == pd.Series\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> zip_counts.shape[0] == 63\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> zip_counts["94103"] == 562\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ hw/hw02/tests/q3ciii.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3ciii',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> type(earliest_date) == pd.Timestamp\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> type(latest_date) == pd.Timestamp\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
