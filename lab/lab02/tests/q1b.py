3acecfedb953b1c317c63ba5cb58927e17296b0c
Dominic Liu <dominiclhx129@gmail.com>
2022-01-28 18:17:47 -0800

    lab02

---
+++ lab/lab02/tests/q1b.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q1b',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> fruit_info["rank2"].dtype\ndtype(\'int64\')', 'hidden': False, 'locked': False},
+                                   {'code': '>>> fruit_info["rank2"] == fruit_info["rank1"]\n0    True\n1    True\n2    True\n3    True\ndtype: bool', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
