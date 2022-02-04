438a71cd7aa8bedab58b76e3fdd26a444cf7d18d
Dominic Liu <dominiclhx129@gmail.com>
2022-02-04 13:47:49 -0800

    lab03 added

---
+++ lab/lab03/tests/q3a.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3a',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> calls_lat_lon.shape\n(2632, 2)', 'hidden': False, 'locked': False},
+                                   {'code': '>>> set(calls_lat_lon.columns) == {"Lat", "Lon"}\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab03/tests/q3c.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3c',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> missing_lat_lon.shape == (20, 15)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> missing_lat_lon["BLKADDR"].isna().sum() == len(missing_lat_lon)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
