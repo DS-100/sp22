19ff9e6f939de43273dae02f7701f35881279c54
Dominic Liu <dominiclhx129@gmail.com>
2022-02-04 13:47:07 -0800

    hw03 updates

--- hw/hw03/tests/q1b.py
+++ hw/hw03/tests/q1b.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q1b',
     'points': [0],
-    'suites': [{'cases': [{'code': ">>> q1b.lower() in ['A', 'B', 'C', 'D']\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': ">>> q1b.lower() in ['A', 'B', 'C', 'D']\nFalse", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
