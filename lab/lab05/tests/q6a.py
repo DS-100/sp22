0c73cb059edc246b6ea86d0c8c8d961875aae9aa
Dominic Liu <dominiclhx129@gmail.com>
2022-02-18 20:46:49 -0800

    lab05

---
+++ lab/lab05/tests/q6a.py
@@ -0,0 +1,7 @@
+test = {   'name': 'q6a',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> np.isclose(mse_tips_linear_list([0, 0]), 10.896283606557375)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
