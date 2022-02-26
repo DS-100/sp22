26f2acf2703e0e334cc2f7564747956ba3f02031
Dominic Liu <dominiclhx129@gmail.com>
2022-02-25 21:07:17 -0800

    lab06

---
+++ lab/lab06/tests/q3a.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q3a',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> np.isclose(model_multi.intercept_, 56.900099702112954)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.isclose(model_multi.coef_[0], -0.46618963)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
