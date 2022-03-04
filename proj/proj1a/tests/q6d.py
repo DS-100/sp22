d047d352599dd910b67a8bbe8af735d4e10c02f3
Dominic Liu <dominiclhx129@gmail.com>
2022-03-04 14:34:35 -0800

    proj1a

---
+++ proj/proj1a/tests/q6d.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q6d',
+    'points': 2,
+    'suites': [   {   'cases': [   {'code': '>>> len(find_expensive_neighborhoods(training_data, 5, np.median)) == 5\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> isinstance(expensive_neighborhoods, list)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ proj/proj1a/tests/q7b.py
@@ -0,0 +1,11 @@
+test = {   'name': 'q7b',
+    'points': 2,
+    'suites': [   {   'cases': [   {   'code': ">>> expected_ohe_cols = set(['x0_Other', 'x0_Shake', 'x0_Shingle/Asphalt' ,'x0_Slate', 'x0_Tar&Gravel', 'x0_Tile'])\n"
+                                               '>>> expected_ohe_cols.issubset(set(training_data.columns)) == True\n'
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
