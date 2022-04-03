4481f4ff3d3c740d416a5445543c4f1286bc59bc
Dominic Liu <dominiclhx129@gmail.com>
2022-04-03 00:06:03 -0700

    lab 10 again

---
+++ lab/lab10/tests/q2.py
@@ -0,0 +1,23 @@
+test = {   'name': 'q2',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> res_q2.shape == (12, 2)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': ">>> list(res_q2['total']) == [21665, 13830, 12175, 11028, 6995, 4123, 2747, 1558, 623, 410, 66, 6]\nTrue", 'hidden': False, 'locked': False},
+                                   {   'code': ">>> list(res_q2['category']) == ['actor',\n"
+                                               "...  'writer',\n"
+                                               "...  'actress',\n"
+                                               "...  'producer',\n"
+                                               "...  'director',\n"
+                                               "...  'composer',\n"
+                                               "...  'cinematographer',\n"
+                                               "...  'editor',\n"
+                                               "...  'self',\n"
+                                               "...  'production_designer',\n"
+                                               "...  'archive_footage',\n"
+                                               "...  'archive_sound']\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
