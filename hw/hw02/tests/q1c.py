81f5750abb121b1df294a7b317dfffe7c6180ebb
Rohan Jha <rohan.jha@berkeley.edu>
2022-07-17 14:59:47 -0700

    added hw2

--- hw/hw02/tests/q1c.py
+++
@@ -1,3 +0,0 @@
-test = {   'name': 'q1c',
-    'points': [0],
-    'suites': [{'cases': [{'code': '>>> q1c in set(["A", "B", "C"])\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- hw/hw02/tests/q2ci.py
+++
@@ -1,9 +0,0 @@
-test = {   'name': 'q2ci',
-    'points': 1,
-    'suites': [   {   'cases': [   {   'code': '>>> valid_zips = valid_zips.astype("string")\n>>> assert pd.api.types.is_string_dtype(valid_zips)\n>>> type(valid_zips) == pd.Series\nTrue',
-                                       'hidden': False,
-                                       'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
