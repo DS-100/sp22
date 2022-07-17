f75c917ff053a7a509715911fbd19e4180dd5277
Rohan Jha <rohan.jha@berkeley.edu>
2022-07-17 16:03:01 -0700

    updated hw7

--- hw/hw07/tests/q1b.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q1b', 'points': 2, 'suites': [{'cases': [{'code': '>>> res_q1b.shape == (103, 2)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
